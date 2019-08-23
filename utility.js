9abcf143c2769c5b31e54490e9f5304147b4c9eb
Igor Ozheredov <o.igorok@gmail.com>
2019-08-23 18:03:06 +0300

    homework#03

--- utility.js
+++
@@ -1,6 +0,0 @@
-/* Установить на сайте красную рамку для всех html-тегов(нужно выполнить в консоли)
- */
-Array.from(document.querySelectorAll('*'),
-            elem => elem.style.cssText
-            = 'outline: 1px solid red;'
-            );
