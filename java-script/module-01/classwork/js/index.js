f3bfff0f9d001b9f77ef0368c830e936276629d6
KuYalda <o.igorok@gmail.com>
2019-10-02 17:12:15 +0300

    start java-script

---
+++ java-script/module-01/classwork/js/index.js
@@ -0,0 +1,118 @@
+/* console.log('work');
+
+const PI_CASE = 3.14;
+const a = 5 + 1;
+let b = a + 6;
+
+console.log(b);
+
+b = b + 10;
+
+console.log('b:', b);
+
+console.log(typeof b);
+console.log(typeof "Word");
+console.log(typeof g);
+
+b = '5' + b;
+
+console.log('b:', b);
+
+b = Number(b);
+
+b = b + 10;
+
+console.log(b);
+
+console.log('type of b:', typeof b);
+
+console.log('String b:', String(b));
+
+console.log('5 == 5', 5 == 5);
+console.log('5 == 5', 5 == '5');
+
+console.log('5 === 5', 5 === 5);
+console.log('5 === 5', 5 === '5');
+
+console.log('5 !== 5', 5 !== '5');
+console.log('5 !== 5', 5 !== 5);
+
+let message = 'Some word';
+
+console.log("'Some word':", "Some word".toLocaleLowerCase());
+
+console.log("'Some word':", message.toUpperCase());
+
+console.log(message.length);
+
+message = message.toUpperCase();
+
+console.log(message, typeof(message));
+
+console.log(message[3]);
+console.log(message[message.length]);
+
+console.log(message[message.length-1]);
+
+console.log(Number.parseInt('12.123 number'));
+
+console.log(Number.parseFloat('12.123number'));
+
+console.log(Number.parseFloat('12,123number'));
+
+console.log(Number('12.123number'));
+console.log(NaN === NaN);
+console.log('Number.isNaN() :', isNaN(Number('message.length')));
+ */
+/* 
+let cash = 1000;
+const price= 500;
+
+if(cash >= price) {
+    alert('success');
+} else {
+    alert('no cash');
+} */
+/* 
+if('number' && 0 || 100) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+const userInput = prompt('What is your name?');
+console.log(userInput);
+
+if(userInput && userInput !== '') {
+    alert('Welcome ' + userInput);
+} else {
+    alert('Enter your name');
+} */
+/* 
+const cash = +prompt('Have many cash do you have?');
+const credit = 1000;
+const price = 800;
+
+if(cash >= price) {
+    console.log('pay with cash!');
+} else if (cash >= credit){
+    console.log('pay with credit');
+} else {
+    console.log('check your balance');
+}
+
+console.log(cash); */
+/* 
+const day = prompt('What is the day now?');
+
+switch(day) {
+    case 'monday':
+        alert('Good morning');
+        break;
+    case 'thursday':
+        alert('Sorry. We don\'t work');
+        break;
+    default:
+        alert('Are you stupid?');
+} */
+
