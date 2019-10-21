d2f91930ec3a457ec2d130ef1b95101831f12ece
KuYalda <o.igorok@gmail.com>
2019-10-21 12:38:35 +0300

    js cw#7 rev.4

--- java-script/module-07/classwork/js/index.js
+++ java-script/module-07/classwork/js/index.js
@@ -13,7 +13,7 @@ const url = document.querySelector('.users__list');
 function renderUsers(arrUsers, ref) {
   const markup = arrUsers.reduce((acc, el) => {
     const li = `
-      <li class ='users__item'>
+      <li class ='users__item' data-modal='${el.about}'>
         <img class='users__img' src=${el.picture} />
         <p class='users__name'>${el.name}</p>
         <span class='users__email'>${el.email}</span>
@@ -27,19 +27,42 @@ function renderUsers(arrUsers, ref) {
 
 renderUsers(users, url);
 
+const modalWrapper = document.querySelector('.modal__wrapper');
+const modalTitle = modalWrapper.querySelector('.modal__title');
+
+modalWrapper.addEventListener('click', (e) => {
+  if(e.target.nodeName === 'BUTTON' || 
+  e.target === e.currentTarget
+  ) {
+    modalWrapper.classList.remove('active');
+  } //else if(e.currentTarget === modalWrapper) {
+    // modalWrapper.classList.remove('active');
+
+  // }
+})
+
+
 url.addEventListener('click', (e) => {
   console.log('e.target', e.target);
   console.log('e.currentTarget', e.currentTarget);
-  console.log('e.target.nodeName', e.target.nodeName);
-  console.log('e.target.parentNode', e.target.parentNode)
+  // console.log('e.target.nodeName', e.target.nodeName);
+  // console.log('e.target.parentNode', e.target.parentNode)
 
   if(
     e.target.nodeName === 'BUTTON' && 
     e.target.dataset.action === 'DEL') {
       e.target.closest('.users__item').remove();
+  } else if(e.target.nodeName === 'LI') {
+    // alert('this is LI')
+    modalWrapper.classList.add('active');
+    const discription = e.target.dataset.modal;
+    modalTitle.textContent = discription;
   }
 });
 
-console.log(
-  url.contains(document.querySelector('.users__item'))
-);
+
+
+// console.log(
+//   url.contains(document.querySelector('.users__item'))
+// );
+
