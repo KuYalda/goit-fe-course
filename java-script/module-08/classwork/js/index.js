cdeb3c0284b1a5f5fb341de2b13735ca3f93cee3
KuYalda <o.igorok@gmail.com>
2019-10-23 17:10:05 +0300

    js cw#09 webpack

--- java-script/module-08/classwork/js/index.js
+++ java-script/module-08/classwork/js/index.js
@@ -1,17 +1,19 @@
-'use strict'
+'use strict';
 
-window.addEventListener(
-  'scroll',
-  _.throttle(() => {
-    console.log('Scroll event handler invocation every 300ms.');
-  }, 300),
-);
+// window.addEventListener(
+//   'scroll',
+//   _.throttle(() => {
+//     console.log('Scroll event handler invocation every 300ms.');
+//   }, 300),
+// );
 
-document.querySelector('input').addEventListener(
-  'input',
-  _.debounce(() => {
-    console.log(
-      'Input event handler invocation after 300ms of inactivity past burst.',
-    );
-  }, 300),
-);
+// document.querySelector('input').addEventListener(
+//   'input',
+//   _.debounce(() => {
+//     console.log(
+//       'Input event handler invocation after 300ms of inactivity past burst.',
+//     );
+//   }, 300),
+// );
+
+console.log('DocumentFragment  :', DocumentFragment);
