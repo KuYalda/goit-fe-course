0f5df5dce41752f2abf66ea411a5d3fbc8334259
Igor Ozheredov <o.igorok@gmail.com>
2019-10-24 22:45:31 +0300

    js hw#10 rev.2

--- java-script/module-10/homework/src/js/menu.js
+++ java-script/module-10/homework/src/js/menu.js
@@ -1,37 +1,5 @@
 import dishes from './menu.json';
 import menuTemplate from '../templates/dish.hbs';
 
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
-
 const menuHTML = menuTemplate(dishes);
 document.getElementById('menu').innerHTML = menuHTML;
-
-const themeControl = document.getElementById('theme-switch-control');
-
-const siteBody = document.getElementsByTagName('body');
-
-if (localStorage.getItem('theme') === 'dark-theme') {
-  siteBody[0].classList.add(Theme.DARK);
-  themeControl.checked = true;
-} else {
-  siteBody[0].classList.add(Theme.LIGHT);
-  localStorage.setItem('theme', `${Theme.LIGHT}`);
-}
-
-// console.log(activeTheme);
-
-themeControl.addEventListener('change', () => {
-  // console.dir(themeControl);
-  if (themeControl.checked) {
-    siteBody[0].classList.toggle(Theme.LIGHT);
-    siteBody[0].classList.add(Theme.DARK);
-    localStorage.setItem('theme', `${Theme.DARK}`);
-  } else {
-    siteBody[0].classList.add(Theme.LIGHT);
-    siteBody[0].classList.toggle(Theme.DARK);
-    localStorage.setItem('theme', `${Theme.LIGHT}`);
-  }
-});
