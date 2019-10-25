dc184a560392e894f487f751f9d066b0cd8cc272
KuYalda <o.igorok@gmail.com>
2019-10-25 16:53:19 +0300

    js cw#10 start

---
+++ java-script/module-11/classwork/src/index.js
@@ -0,0 +1,35 @@
+import './styles.css';
+
+// const interval = 10000;
+// const timeOut = 20000;
+
+// setTimeout(() => {
+//   alert('clg1');
+// }, interval);
+
+// console.log('clg2');
+// console.log('clg3');
+
+// const timerId = setInterval(() => {
+//   console.log('interval', new Date());
+//   const DateNow = new Date();
+// }, 20000);
+
+// setTimeout(() => {
+//   alert('timerId');
+//   clearInterval(timerId);
+// }, 4000);
+
+const myPromis = new Promise((resolve, reject) => {
+  if (true) {
+    resolve("it's true");
+  } else {
+    reject("it's false");
+  }
+});
+
+// console.dir(myPromis);
+// console.dir(Promise);
+// console.dir(document);
+
+console.dir(document.styleSheets[0]);
