0cb782de3c08bc772cbc809cd9ebd4744fd00daf
Igor Ozheredov <o.igorok@gmail.com>
2019-11-15 22:23:57 +0200

    Updates

---
+++ precache-manifest.5df90eca5fd4cbe1dfd1fa3ba39e91eb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f99bc3a69d1d31f9a81fbe9f28e715",
+    "url": "/goit-fe-course/index.html"
+  },
+  {
+    "revision": "6726acd4a348b85fdc2f",
+    "url": "/goit-fe-course/static/js/2.bebb05e0.chunk.js"
+  },
+  {
+    "revision": "5a5602f996abd7e79ac9",
+    "url": "/goit-fe-course/static/js/main.8337572f.chunk.js"
+  },
+  {
+    "revision": "7d86e1df8110149c1fe4",
+    "url": "/goit-fe-course/static/js/runtime-main.37d7a590.js"
+  }
+]);
