b7ad7a66d8619ba4d17b9a48a3f52617fb4300a9
KuYalda <o.igorok@gmail.com>
2019-11-22 13:33:36 +0200

    react hw#2 add // eslint-disable-next-line

--- react/homework-02/src/components/Cabinet/Balance/index.js
+++ react/homework-02/src/components/Cabinet/Balance/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Balance = ({ balance, deposit, withdraw }) => (
   <>
+    // eslint-disable-next-line
     <span>⬆️{deposit}$</span>
+    // eslint-disable-next-line
     <span>⬇️{withdraw}$</span>
     <span>Balance: {balance}$</span>
   </>
