182ae457f85ed1ccc8d198051044b753214170dd
KuYalda <o.igorok@gmail.com>
2019-11-25 14:33:59 +0200

    Updates

---
+++ precache-manifest.72ef5c377c1e86a848f3b379004069ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5be4051c8923ad39632a45b6ce0dcb5",
+    "url": "/goit-fe-course/index.html"
+  },
+  {
+    "revision": "2f2522e5ff75e7aaded8",
+    "url": "/goit-fe-course/static/css/main.2e04b7fc.chunk.css"
+  },
+  {
+    "revision": "7fab8a2dd7df8ee85542",
+    "url": "/goit-fe-course/static/js/2.d7bf9c9e.chunk.js"
+  },
+  {
+    "revision": "2f2522e5ff75e7aaded8",
+    "url": "/goit-fe-course/static/js/main.81343ba8.chunk.js"
+  },
+  {
+    "revision": "c7dcc79a86f180779a1e",
+    "url": "/goit-fe-course/static/js/runtime-main.c1f6d307.js"
+  }
+]);
