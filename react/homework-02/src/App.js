c8fecc723ec7c69dc7d810a4c5186b87b2ad96f3
KuYalda <o.igorok@gmail.com>
2019-11-25 14:31:51 +0200

    hw#2-1 change focus

--- react/homework-02/src/App.js
+++ react/homework-02/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Reader from './components/Reader';
-import Dashboard from './components/Cabinet/Dashboard';
 import article from './db/publications.json';
 import './App.module.css';
 
 const App = () => (
   <>
     <Reader items={article} />
-    <Dashboard />
   </>
 );
 
